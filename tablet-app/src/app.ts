8c4daab9e3eb7fdd0d3186d444447822c93a7ce1
sigjig <sigmundklaa@outlook.com>
2020-01-05 14:55:23 +0100

    uhh

--- tablet-app/src/app.ts
+++ tablet-app/src/app.ts
@@ -1,12 +1,21 @@
 
+import path from 'path'
 import { app, BrowserWindow } from 'electron'
 
 function create() {
     const window = new BrowserWindow({
         width: 900,
         height: 600,
-        titleBarStyle: 'hiddenInset'
+        titleBarStyle: 'hidden',
+        backgroundColor: '#111',
+        show: false,
     })
+
+    window.maximize()
+    window.removeMenu()
+    window.loadFile(path.join(process.cwd(), 'static', 'html', 'control.html'))
+
+    window.once('ready-to-show', window.show)
 }
 
 app.on('ready', create)
