0b1579cf8381165a3679e677fcdcb5e20984c087
sigjig <sigmundklaa@outlook.com>
2020-02-01 13:48:48 +0100

    Hmm

--- strip/src/main.cpp
+++ strip/src/main.cpp
@@ -1,7 +1,7 @@
 
 #include <Arduino.h>
-// #include <SPI.h>
-// #include <WiFiNINA.h>
+#include <SPI.h>
+#include <WiFiNINA.h>
 
 #include "../config.h"
 
@@ -13,42 +13,58 @@ const char* password = NETWORK_PASS;
 #include "strip.h"
 #include "cyclehandler.h"
 
-#define NUM_STRIPS 2
+ColorPin pins[] = {3, 5, 6};
+RGBStrip strip(pins);
 
-ColorPin s1[] = {3, 5, 6};
-ColorPin s2[] = {9, 10, 11};
+uint8_t led_pins[] = {9, 10, 11};
 
-RGBStrip strips[NUM_STRIPS] = {s1, s2};
+void led_write(uint8_t bytes)
+{
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        digitalWrite(led_pins[i], (bytes >> i) & 0x01);
+    }
+}
 
 void setup()
 {
     Serial.begin(BAUD_RATE);
 
     Serial.println("Connecting to Wifi");
-    /* WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
 
     for (int time = millis(); WiFi.status() != WL_CONNECTED; time = millis())
     {
-      if ((time % 2000) == 0) Serial.println("Connecting...");
+        if ((time % 2000) == 0) Serial.println("Connecting...");
     }
- */
+
     Serial.println("Wifi connection established");
+
+    for (uint8_t i = 0; i < 3; i++) pinMode(led_pins[i], OUTPUT);
+
+    led_write(0xFF);
 }
 
 void loop()
 {
     static bool is_red = false;
+    static uint8_t iteration = 0;
 
     if (millis() % 4000)
     {
-      Serial.println("Intializing change");
-      auto rgb = (is_red ? RGB({0, 170, 255}) : RGB({240, 0, 0}));
+        Serial.println("Intializing change");
+        auto rgb = (is_red ? RGB({0, 170, 255}) : RGB({240, 0, 0}));
 
-      is_red = !is_red;
-
-      for (uint8_t i = 0; i < 2; i++) strips[i].commit_rgb(rgb);
+        is_red = !is_red;
+        
+        strip.commit_rgb(rgb);
     }
 
+    iteration = (iteration + 1) % 3;
+
+    led_write(0x01 << iteration);
+
     cycle_handler.run();
+    
     delay(1);
 }
