7e4ba1cf7b391a0242528415e3d511a0506d3f0a
sigjig <sigmundklaa@outlook.com>
2020-02-19 19:26:56 +0100

    Webapp added

---
+++ webapp/src/screens/devices.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+export default class Devices extends React.Component<{}, {}>
+{
+    render()
+    {
+        return (
+            <div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        )
+    }
+}
